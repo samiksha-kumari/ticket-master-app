64dfcd67aaedbe67571ea197da5779903f08f225
samiksha-kumari <samiksha-kumari_outlook.com>
2020-01-21 21:31:48 +0530

    api changes

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 const axios = Axios.create({
   // Axios - method - is an object
-  baseURL: "http://localhost:3010"
+  baseURL: "http://localhost:3010/api"
   // "/"
 });
 export default axios;
